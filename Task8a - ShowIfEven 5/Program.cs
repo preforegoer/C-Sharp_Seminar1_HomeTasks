4e43444c7a2be45beaad76d27503290d39947312
preforegoer <123918009+preforegoer@users.noreply.github.com>
2023-02-04 10:16:34 +0600

    Add files via upload

---
+++ Task8a - ShowIfEven 5/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+// Console.Write("Enter a Number: ");
+// int a = Convert.ToInt32(Console.ReadLine());
+
+int a = 5;
+Console.WriteLine($"All Even numbers from 0 to {a}: ");
+
+for (int i = 2 ; i <=a; i++)
+if (i % 2 ==0)
+    Console.Write($" {i}");
+else
+    Console.Write(";");
