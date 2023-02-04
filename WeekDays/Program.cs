a42731db3a008489b010e1c0fe4fc48bf82a3267
Vladimir.Sergeyev <preforegoer@gmail.com>
2023-02-04 10:03:09 +0600

    Added All Tasks

---
+++ WeekDays/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number of Week day: ");
+int v = Convert.ToInt32(Console.ReadLine());
+
+if (v < 1 || v > 7)
+{
+    Console.Write("There are only 7 days in a week, Please enter a number from 1 to 7: ");
+    int v = Convert.ToInt32(Console.ReadLine());
+}
+if (v == 1)
+    Console.Write("The first day of week is Monday");
+
+else if (v == 2)
+    Console.Write("The second day of week is Tuesday");
+
+else if  (v == 3)
+    Console.Write("The third day of week is Wendnesday");
+
+else if  (v == 4)
+    Console.Write("The first day of week is Monday");
+
+else if  (v == 5)
+    Console.Write("The fifth day of week is Friday");
+
+else if  (v == 6)
+    
+    Console.Write("The first day of week is Saturday");
+
+else
+    Console.Write("The seventh day of week is Sunday");
---
+++ aEqualsToBMultipliedByB/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Clear();
+
+Console.Write("Enter First Number: ");
+double a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Second Number: ");
+double b = int.Parse(Console.ReadLine());
+
+if (a==b*b)
+    Console.Write($"{a} equals to {b}*{b}");
+else
+    Console.Write($"{a} does not equal to {b}*{b}");
