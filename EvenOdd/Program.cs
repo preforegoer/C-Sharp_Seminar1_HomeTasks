9bed6831fa4d5cb6dc897a9abe99b33a19d0bfea
preforegoer <123918009+preforegoer@users.noreply.github.com>
2023-02-04 10:12:56 +0600

    Add files via upload

---
+++ EvenOdd/Program.cs
@@ -0,0 +1,10 @@
+﻿// OddOrEven
+Console.Clear();
+
+Console.Write("Enter a number: ");
+int a = Convert.ToInt32(Console.ReadLine()), count = (-1) * a;
+
+if (a%2 == 1)
+    Console.WriteLine($"{a} is Odd");
+else 
+    Console.WriteLine($"{a} is Even");
---
+++ NumbersEntry/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.Write("Enter First Number: ");
+double a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Enter Second Number: ");
+double b = int.Parse(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine($"{a} is the Maximum");
+}
+else if (a < b)
+{
+    Console.WriteLine($"{b} is the Maximum");
+}
+else
+{
+    Console.WriteLine("The numbers are equal");
+}
